3fe08d1b1aad3d73e19c38476c2a0a68688ab3fc
Jeremy-Lu-APComp <21jlu@athenian.org>
2021-02-09 00:01:21 +0000

    first commit - incomplete

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,7 +4,10 @@ public class MyMain {
 
     // Sorts the ArrayList using insertion sort
     public static ArrayList<Integer> insertionSort(ArrayList<Integer> list) {
-        // YOUR CODE HERE
+        ArrayList <String> output_array = new ArrayList<String>();
+        for(int i = 0; i < list.size(); i++){
+            
+        }
         return null;
     }
 
@@ -12,11 +15,30 @@ public class MyMain {
      // sorted array
      // You may assume arr1 and arr2 are the same length
     public static int[] merge(int[] arr1, int[] arr2) { 
-        // YOUR CODE HERE
-        return null;
+        int[] combined = new int[arr1.length + arr2.length];
+        int arraypos = 0;
+        int arraypos_2 = 0;
+        int mergedpos = 0;
+    while(arraypos < arr1.length && arraypos_2 < arr2.length) {
+        if (arr1[arraypos] < arr2[arraypos_2]) {
+            combined[mergedpos+=1] = arr1[arraypos+=1];
+        } 
+        else {
+            combined[mergedpos+=1] = arr2[arraypos_2+=1];
+        }
     }
+    while (arraypos < arr1.length) {
+            combined[mergedpos+=1] = arr1[arraypos+=1];
+        }
+    while (arraypos_2 < arr2.length) {
+            combined[mergedpos+=1] = arr2[arraypos_2+=1];
+        }
+    return combined;
+}
 
     public static void main(String[] args) {
-        // You can test your code here
+        int[] one = { 3, 7, 10 };
+        int[] two = { 4, 8, 11 };
+        System.out.println(merge(one, two));
     }
 }
