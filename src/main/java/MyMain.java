a68f539b36d8b137993d292ee81486c65b545b96
Jeremy-Lu-APComp <21jlu@athenian.org>
2021-02-09 22:26:35 +0000

    Third commit

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-
+import java.util.Arrays;
 public class MyMain {
 
     // Sorts the ArrayList using insertion sort
@@ -39,23 +39,36 @@ public class MyMain {
     int[] output_array = new int[arr1.length + arr2.length];
     int length_1 = arr1.length;
     int length_2 = arr2.length;
-    for (int i = 0; i < length_1; i++){
+    int i, j;
+    i = j = 0;
+    while(i < length_1 && j < length_2){
+    int var_1 = arr1[i];
+    int var_2 = arr2[j];
+    if(var_1>var_2){
+        output_array[i+j] = var_2;
+        j++;
+    }
+    else if(var_2>var_1){
+        output_array[i+j] = var_1;
+        i++;
+    }
+}
+    while(i<length_1){
         int var_1 = arr1[i];
-    for (int a = 0; a < length_2; a++){
-        int var_2 = arr2[i];
-        if(var_1>var_2){
-            output_array[i] = var_2;
-        }
-        else if(var_2>var_1){
-            output_array[i] = var_1;
-        }
+        output_array[i+j] = var_1;
+        i++;
     }
+    while(j<length_2){
+        int var_2 = arr2[j];
+        output_array[i+j] = var_2;
+        j++;
     }
+    return output_array;
 }
 
     public static void main(String[] args) {
         int[] one = { 3, 7, 10 };
         int[] two = { 4, 8, 11 };
-        System.out.println(merge(one, two));
+        System.out.println(Arrays.toString(merge(one, two)));
     }
 }
